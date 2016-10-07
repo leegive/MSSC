ef03ddd8f2a26a43d2b27ae84fecbb62fe1b9a33
leegive <leegive@icloud.com>
2016-10-07 16:51:39 +0800

    routine commit

--- demo-service/src/main/java/com/leegive/controller/DefaultController.java
+++ demo-service/src/main/java/com/leegive/controller/DefaultController.java
@@ -1,12 +1,14 @@
 package com.leegive.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by leegive on 07/10/2016.
  */
+@RefreshScope
 @RestController
 @RequestMapping("/")
 public class DefaultController {
